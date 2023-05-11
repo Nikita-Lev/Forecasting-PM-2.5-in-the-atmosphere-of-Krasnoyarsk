19c20f1086dff2516ede8fe3317f26bea853a8bc
Nikita-Lev <121679347+Nikita-Lev@users.noreply.github.com>
2023-05-11 21:15:17 +0700

    Add files via upload
    Изменена структура программы для упрощения прогнозирования, добавлена возможность сохранить результаты и модель.
    Создан класс Forecaster, атрибутами которого являются временные периоды прогнозирования, тип датчиков, а также район прогнозирования.
    Само прогнозирование выполняется в 3 ячейки:
    1) Сначала срезаются данные, определяются временные периоды
    2) Создаётся объект класса, строятся ACF, PACF для определения начальных приближений гиперпараметров
    3) Обучается модель с оптимальными или заданными параметрами (можно выбрать)

--- PredFunctions.py
+++ PredFunctions.py
@@ -13,10 +13,12 @@ from statsmodels.tsa.stattools import adfuller
 from scipy import stats
 from datetime import datetime, timedelta
 
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_absolute_percentage_error as smape
 
 from itertools import product
 from tqdm.notebook import tqdm
+
+import os 
 
 
 # ### Функции дифференцирования ряда и подбора гиперпараметров ARIMA
@@ -118,14 +120,13 @@ def optimize_SARIMA(data, p, d, q, P=0, D=0, Q=0, s=0):
     warnings.filterwarnings("ignore")
     for param in tqdm(parameters_list):
         try:
-            # Сезонная модель
+            # SARIMA
             if s:
                 model = sm.tsa.statespace.SARIMAX(data, order=(param[0], param[1], param[2]),
-                                              seasonal_order=(param[3], param[4], param[5], s),
-                                             enforce_stationarity=False).fit(disp=-1)
+                                              seasonal_order=(param[3], param[4], param[5], s)).fit(disp=-1)
+            # ARIMA
             else:
-                model = sm.tsa.statespace.SARIMAX(data, order=(param[0], param[1], param[2]),
-                                             enforce_stationarity=False).fit(disp=-1)
+                model = sm.tsa.statespace.SARIMAX(data, order=(param[0], param[1], param[2])).fit(disp=-1)
         
         except ValueError:
             continue
@@ -136,14 +137,9 @@ def optimize_SARIMA(data, p, d, q, P=0, D=0, Q=0, s=0):
         if aic < best_aic:
             best_aic = aic
             best_param = param
-        results.append([param, model.aic])
-        
-    result_table = pd.DataFrame(results)
-    result_table.columns = ['parameters', 'aic']
-    #Sort in ascending order, lower AIC is better
-    result_table = result_table.sort_values(by='aic', ascending=True).reset_index(drop=True)
     
-    return result_table['parameters'][0]
+    print('Оптимальные гиперпараметры', best_param)
+    return best_param
 
 
 # In[12]:
@@ -159,7 +155,7 @@ def getModel(data, param, s):
     
     display(model.summary().tables[1]) # Таблица коэффициентов
     
-    model.plot_diagnostics(figsize=(14, 12)) # Характеристика модели
+    model.plot_diagnostics(figsize=(14, 12)) # ,Диагностические графики
     plt.show()
     
     return model
@@ -186,18 +182,6 @@ def BoxCox(data):
 # In[14]:
 
 
-# Сравнение двух графиков вместе
-def CompareGraph(y1, y2, l1, l2, title):
-    plt.figure(figsize = (13, 6))
-    plt.plot(y1, label = l1)
-    plt.plot(y2, label = l2)
-
-    plt.gca().set(title = title)
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-
 # In[15]:
 
 
@@ -209,30 +193,18 @@ def ReIndex(data, h):
     return data
 
 
-# Сравнение значений, полученных моделью с истинными
-def CompareModel(realData, model,lmbda = False):
-    modVal = ReIndex(model.fittedvalues, 1)
+# Вычисление метрик качества прогнозов
+def Metrics(y, y_pred):
     
-    # Если применено преобразование Бокса-Кокса, обратить
-    if lmbda != False:
-        realData = invboxcox(realData, lmbda)
-        modVal = invboxcox(modVal, lmbda)
+    mae = round(np.mean(np.abs(y - y_pred)), 4)
+    R2 = round(r2_score(y, y_pred), 4)
+    sMAPE = round(100 * smape(y, y_pred), 2)
+    wape = round(100 * np.sum(np.abs(y - y_pred)) / np.sum(np.abs(y)), 2)
     
-    CompareGraph(realData, modVal, 'Исходные данные', 'Данные, описываемые моделью', 'Сравнение истинных значений с полученными моделью')
+    return(pd.DataFrame({'Метрика' : ['MAE', '$R^2$', 'SMAPE', 'WAPE'], 'Значение': [mae, R2 , f'{sMAPE} %' , f'{wape} %']}))
     
-    mae = np.mean(np.abs(realData - modVal))
-    R2 = r2_score(realData[:-1], modVal)
-    print('MAE:', mae)
-    print('R2:', R2)
 
-# Сравнение предсказаний с истинными значениями
-def ComparePred(realData, pred):
-    CompareGraph(realData, pred, 'Истинные данные', 'Прогнозируемые данные', f'Сравнение прогнозов с истинными данными')
-    
-    mae = np.mean(np.abs(realData - pred))
-    R2 = r2_score(realData, pred)
-    print('MAE:', mae)
-    print('R2:', R2)
+
 
 # In[16]:
 
@@ -243,4 +215,102 @@ def GetPred(model, start, end, lmbda = False):
     if lmbda != False:
         pred = invboxcox(pred, lmbda)
     return pred
+    
+    
+    
+# Класс прогнозирования
+class Forecaster:
+    
+    def __init__(self, sensor, district, begin, start, end):
+        self.district = district
+        # Срез данных от begin до end
+        self.begin = begin
+        self.start = start
+        # Начало прогнозирования
+        self.end = end
+        
+        # Датчики (КНЦ или министерские)
+        self.sensor = sensor
+        
+        # Данные
+        self.df = pd.read_csv(f'pm25_{self.sensor}.csv', sep = ';', index_col = ['Date'], parse_dates = ['Date'])[district]
+        
+        # Тренировочная и тестовая выборки
+        self.train = self.df[begin : start]
+        self.test = self.df[start : end]
+        
+        # Отображение ACF, PACF
+        plotCF(self.train)
 
+      
+    # Сравнение двух функций на одном макете
+    def CompareGraph(self, y1, y2, l1, l2, title):
+        plt.figure(figsize = (13, 6))
+        plt.plot(y1, label = l1)
+        plt.plot(y2, label = l2)
+
+        plt.title(f'Сравнение истинных и прогнозируемых значений {title}. ARIMA{self.optParam}')
+        plt.legend()
+        plt.grid()
+        
+        # Вычисление метрик
+        metrics = Metrics(y1, y2)
+        
+        plt.table(cellText = metrics.values, colLabels = metrics.columns, loc='bottom')
+        plt.tick_params(axis='x', pad=-15)
+        plt.subplots_adjust(bottom=0.15)
+        
+        if self.save == True:
+            
+            self.path = f'{self.district}_{self.sensor}/{self.begin} — {self.end}'
+            
+            if not os.path.exists(self.path):
+                os.mkdir(self.path)
+            plt.savefig(f'{self.path}/{title}.png')   
+        
+        plt.show()
+
+       
+    # Сравнение значений, полученных моделью с истинными (обучающая выборка)
+    def TrainCompare(self, lmbda = False):
+        self.modVal = ReIndex(self.model.fittedvalues, 1)
+
+        # Если применено преобразование Бокса-Кокса, обратить
+        if lmbda != False:
+            self.modVal = invboxcox(self.modVal, lmbda)
+
+        self.CompareGraph(self.train[:-1], self.modVal, 'Исходные данные', 'Данные, описываемые моделью', 'train')
+
+    
+
+    # Сравнение предсказаний с истинными значениями (тестовая выборка)
+    def TestCompare(self):
+        self.CompareGraph(self.test, self.predictions, 'Истинные данные', 'Прогнозируемые данные', 'test')
+        
+        
+    # Подбор гиперпараметров, построение модели
+    def getModel(self, p, d, q, use_optimal = True, save = True):
+        self.save = save
+        # Поиск оптимальных гиперпараметров
+        if use_optimal == True:
+            self.optParam = optimize_SARIMA(self.train, p = p , d = d, q = q)
+            self.d = self.optParam[1]
+        else:
+            self.optParam = (p, d, q)
+            self.d = d
+          
+        # Обучение модели и диагностика
+        self.model = getModel(self.train, self.optParam, s = 0)
+
+        # Модель на обучающей выборке
+        self.TrainCompare()
+
+        # Получение прогнозов
+        self.predictions = GetPred(self.model, self.start, self.end)
+
+        # Модель на тестовой выборке
+        self.TestCompare()
+        
+        if self.save == True:
+            self.model.save(f'{self.path}/model.pkl')
+    
